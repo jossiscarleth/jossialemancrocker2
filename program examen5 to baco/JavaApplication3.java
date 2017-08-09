6ca8294277f9a4009e69fed55a43663e71ae264d
jossiscarleth <jossiscarleth@gmail.com>
2017-08-09 08:18:31 -0600

    Add files via upload

---
+++ program examen5 to baco/JavaApplication3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+import java.util.Scanner;
+/**
+ *
+ * @author estudiante
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           String A = "";
+        int B;
+        
+        System.out.println("ingrese SU EDA PRIMERO");
+    
+        
+        
+        
+         System.out.println("ingrese nombre");
+        Scanner D2 = new Scanner (System.in);
+        B = D2.nextInt();
+        
+    }
+    
+}
---
+++ program examen5 to baco/JavaApplication4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+import java.util.Scanner;
+/**
+ *
+ * @author estudiante
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+        public static void main(String[] args) {
+       int A;
+        System.out.println("ingrese valor");
+        Scanner D1 =  new Scanner (System.in);
+        A = D1.nextInt();
+        System.out.println(A*10);
+         System.out.println(A*10);
+          System.out.println(A*10);
+         System.out.println(A*10);
+                 System.out.println(A*10);
+         System.out.println(A*10);
+          System.out.println(A*10);
+         System.out.println(A*10);
+         System.out.println(A*10);
+    }
+    }
+    
+    
+
